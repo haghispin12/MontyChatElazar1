974b67dc425d6ae90d383f4d9dd524ae414b61f9
Elazar <hag.hispin12@gmail.com>
2024-06-26 00:57:35 +0300

    vesion6.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,31 +37,28 @@ android {
 dependencies {
 
     //noinspection GradleCompatible,GradleCompatible,GradleCompatible,GradleCompatible
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-firestore:24.8.1")
+    implementation("com.google.firebase:firebase-firestore:25.0.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
 
     //Rounded ImageView
 
     implementation("com.makeramen:roundedimageview:2.3.0")
     //Firebase
 
-    implementation("com.google.firebase:firebase-messaging-ktx:24.0.0")
+    implementation("com.google.firebase:firebase-messaging-ktx:23.2.1")
     implementation("com.google.firebase:firebase-firestore-ktx:25.0.0")
 
     //MultiDex
     implementation ("androidx.multidex:multidex:2.0.1")
 
     implementation ("com.squareup.picasso:picasso:2.71828")
-
-
-    implementation("androidx.core:core-:1.13.1")
-
 
 
 
